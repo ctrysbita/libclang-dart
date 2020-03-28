80165f4e1756f276d4da4cdb17efe5d0962652b9
Jason C.H <ctrysbita@outlook.com>
2020-03-28 23:01:18 +0800

    Add saveTranslateUnit

--- lib/src/translation_unit.dart
+++ lib/src/translation_unit.dart
@@ -59,4 +59,45 @@ class TranslationUnit {
    * various declarations within the given translation unit.
    */
   Pointer<Cursor> get cursor => _getTranslationUnitCursor(impl);
+
+  static final _getNumDiagnostics = _lib.lookupFunction<
+      Uint32 Function(Pointer<_TranslationUnitImpl>),
+      int Function(Pointer<_TranslationUnitImpl>)>('clang_getNumDiagnostics');
+
+  /**
+   * Determine the number of diagnostics produced for the given
+   * translation unit.
+   */
+  int get numDiagnostics => _getNumDiagnostics(impl);
+
+  static final _saveTranslationUnit = _lib.lookupFunction<
+      Int32 Function(Pointer<_TranslationUnitImpl>, Pointer<Utf8>, Uint32),
+      int Function(Pointer<_TranslationUnitImpl>, Pointer<Utf8>,
+          int)>('clang_saveTranslationUnit');
+
+  /**
+   * Saves a translation unit into a serialized representation of
+   * that translation unit on disk.
+   *
+   * Any translation unit that was parsed without error can be saved
+   * into a file. The translation unit can then be deserialized into a
+   * new \c CXTranslationUnit with \c clang_createTranslationUnit() or,
+   * if it is an incomplete translation unit that corresponds to a
+   * header, used as a precompiled header when parsing other translation
+   * units.
+   *
+   * \param TU The translation unit to save.
+   *
+   * \param FileName The file to which the translation unit will be saved.
+   *
+   * \param options A bitmask of options that affects how the translation unit
+   * is saved. This should be a bitwise OR of the
+   * CXSaveTranslationUnit_XXX flags.
+   *
+   * \returns A value that will match one of the enumerators of the CXSaveError
+   * enumeration. Zero (CXSaveError_None) indicates that the translation unit was
+   * saved successfully, while a non-zero value indicates that a problem occurred.
+   */
+  int saveTranslationUnit(String fileName, int options) =>
+      _saveTranslationUnit(impl, Utf8.toUtf8(fileName), options);
 }
